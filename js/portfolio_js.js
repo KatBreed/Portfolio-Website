910bc1310110a42ddafaafdb0df18271424bc464
Katherine Breed <ksbreed@gmail.com>
2025-03-22 14:41:16 +1300

    Changed slick flexislider to bootstrap carousel

--- js/portfolio_js.js
+++ js/portfolio_js.js
@@ -35,29 +35,3 @@ function enlargeImage(img) {
       document.body.removeChild(overlay);
   });
 }
-
-// Slick Flexislider Carousel
-$(document).ready(function(){
-  $('.slick-carousel').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1, 
-      autoplay: true,    
-      autoplaySpeed: 2000,
-      arrows: true,      
-      dots: true,        
-      responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 2
-              }
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1
-              }
-          }
-      ]
-  });
-});
